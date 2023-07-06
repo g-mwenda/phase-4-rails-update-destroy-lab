d0a1ad9de48ac68e76f338c84b725af4dc7a4938
g-mwenda <gabrielmwenda0001@gmail.com>
2023-07-06 16:15:35 +0300

    First commit

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,38 @@
+class PlantsController < ApplicationController
+
+  # GET /plants
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  # GET /plants/:id
+  def show
+    plant = Plant.find_by(id: params[:id])
+    render json: plant
+  end
+
+  # POST /plants
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
+  end
+
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant, status: :ok
+  end
+
+  def destroy
+    plant = Plant.find(params[:id])
+    plant.destroy
+    render status: :no_content
+  end
+
+  private
+
+  def plant_params
+    params.permit(:name, :image, :price, :is_in_stock)
+  end
+end
